7d66747cd9c864ad0c4d49001f1627d1f9968203
Chun-Hsien Huang <n4m4g64b4@gmail.com>
2020-11-03 16:45:11 +0800

    add docstrings

--- rl_env/005-actor-critic/ac.py
+++ rl_env/005-actor-critic/ac.py
@@ -1,34 +1,76 @@
+from pprint import pprint
 import numpy as np
 import torch
 from torch import nn, optim
 from torch.distributions import Categorical
 import gym
 
-
 class Actor_Net(nn.Module):
+    """action neural network
+
+    Attributes
+    ----------
+    n_features : int
+        number of input feature
+    n_actions : int
+        number of output feature
+
+    Methods
+    -------
+    forward(self, s)
+        get network output
+    """ 
     def __init__(self, n_features, n_actions):
         super(Actor_Net, self).__init__()
         self.f1 = nn.Linear(n_features, 20)
         self.f1.weight.data.normal_(0.0, 0.1)
         self.f1.bias.data.fill_(0.1)
-
         self.relu = nn.ReLU()
-
         self.f2 = nn.Linear(20, n_actions)
         self.f2.weight.data.normal_(0.0, 0.1)
         self.f2.bias.data.fill_(0.1)
 
-        # self.softmax = nn.Softmax(dim=1)
-
     def forward(self, s):
+        """get network output
+        
+        Parameters
+        ----------
+        s : torch.tensor
+            network input (observation)
+
+        Returns
+        -------
+        s : torch.tensor
+            network output (action)
+        """ 
+
         s = self.f1(s)
         s = self.relu(s)
         s = self.f2(s)
-        # acts_prob = self.softmax(s)
-        # return acts_prob
         return s
 
 class Actor():
+    """Agent that generate action from actor network
+
+        Using policy gradient
+
+    Attributes
+    ----------
+    n_features : int
+        number of input feature
+    n_actions : int
+        number of output feature
+    lr : float
+        learning rate
+
+    Methods
+    -------
+    learn(self, s, a, td)
+        update actor network
+    choose_action(self, s)
+        choose action from actor network
+    """ 
+
     def __init__(self, n_features, n_actions, lr=0.001):
         self.n_features = n_features
         self.net = Actor_Net(n_features, n_actions)
@@ -36,6 +78,17 @@ class Actor():
         self.criterion = nn.CrossEntropyLoss(reduction='none')
 
     def learn(self, s, a, td):
+        """optimize actor network
+
+        Parameters
+        ----------
+        s : list[float]
+            environment state
+        a : int
+            action
+        td : float
+            critic give td error to help actor learning
+        """ 
         s = torch.tensor(s, dtype=torch.float).unsqueeze(0)
         assert s.shape == torch.zeros((1, self.n_features)).shape
         a = torch.tensor([a], dtype=torch.long)
@@ -53,8 +106,19 @@ class Actor():
         loss.backward()
         self.optimizer.step()
 
-
     def choose_action(self, s):
+        """choose action from actor network
+
+        Parameters
+        ----------
+        s : list[float]
+            environment state
+
+        Returns
+        -------
+        a : int
+            action
+        """
         s = torch.tensor(s, dtype=torch.float).unsqueeze(0)
         out = self.net(s)
         probs = nn.Softmax(dim=1)(out)
@@ -69,9 +133,7 @@ class Critic_Net(nn.Module):
         self.f1 = nn.Linear(n_features, 20)
         self.f1.weight.data.normal_(0.0, 0.1)
         self.f1.bias.data.fill_(0.1)
-
         self.relu = nn.ReLU()
-
         self.f2 = nn.Linear(20, 1)
         self.f2.weight.data.normal_(0.0, 0.1)
         self.f2.bias.data.fill_(0.1)
@@ -83,6 +145,11 @@ class Critic_Net(nn.Module):
         return s
 
 class Critic():
+    """Determine the value function
+        
+        Value function approximation
+        Give td error to actor to optimize choosing the action
+    """
     def __init__(self, n_features, lr=0.01, GAMMA=0.9):
         self.n_features = n_features
         self.GAMMA = GAMMA
@@ -96,8 +163,27 @@ class Critic():
         assert s_.shape == torch.zeros((1, self.n_features)).shape
         r = torch.tensor(r, dtype=torch.float)
 
+        """
+        Temporal Difference (TD)
+
+        TD(1)
+        -----
+        Gt = r(t+1) + gamma*r(t+2) + ...gamma**(T-1)*r(t+T)
+        loss = Gt - v(St)
+
+        TD(0)
+        ----
+        Instead of using the accumulated sum of discounted rewards (Gt)
+        we will only look at the immediate reward (Rt+1), 
+        plus the discount of the estimated value of only 1 step ahead (V(St+1))
+
+        r(t+1) + gamma * v(St+1)
+        loss = r(t+1) + gamma * v(St+1) - v(St)
+        """
+
         v_ = self.net(s_)
-        td_err = r + self.GAMMA * v_ - self.net(s)
+        v = self.net(s)
+        td_err = r + self.GAMMA * v_ - v 
         loss = torch.square(td_err)
 
         self.optimizer.zero_grad()
@@ -123,8 +209,13 @@ def main():
     env.seed(1)  # reproducible
     env = env.unwrapped
     
+    pprint(env.__dict__)
     N_F = env.observation_space.shape[0]
     N_A = env.action_space.n
+
+    # print(env.observation_space.high)
+    # print(env.observation_space.low)
+    # print(N_F, N_A)
 
     actor = Actor(N_F, N_A, LR_A)
     critic = Critic(N_F, LR_C, GAMMA)
