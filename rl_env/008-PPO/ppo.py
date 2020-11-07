71d95cbe785105f2e58a0b7675b90897d9bd0901
Chun-Hsien Huang <n4m4g64b4@gmail.com>
2020-11-08 00:08:48 +0800

    bug: could not learn

--- rl_env/008-PPO/ppo.py
+++ rl_env/008-PPO/ppo.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from torch import nn, optim
+import matplotlib.pyplot as plt
 import gym
 
 EP_MAX = 1000
@@ -13,6 +14,24 @@ A_UPDATE_STEPS = 10
 C_UPDATE_STEPS = 10
 EPSILON=0.2
 
+"""
+1. Build critic net
+    - net
+    - loss
+    - opt
+2. Build target actor net, eval actor net
+    - 2 net
+    - choose action
+    - loss
+    - opt
+3. Update
+    - copy state dict
+    - advantage from critic
+    - opt step actor
+    - opt step critic
+
+"""
+
 class Critic_Net(nn.Module):
     def __init__(self, s_dim):
         super(Critic_Net, self).__init__()
@@ -21,7 +40,7 @@ class Critic_Net(nn.Module):
             nn.ReLU(),
             nn.Linear(100, 1)
             )
-    def forward(x, self):
+    def forward(self, x):
         return self.net(x)
 
 class Actor_Net(nn.Module):
@@ -41,22 +60,99 @@ class Actor_Net(nn.Module):
 
         return mu, sigma
 
+
+
+class PPO:
+    def __init__(self, s_dim, a_dim):
+        self.critic = Critic_Net(s_dim)
+        self.opt_c = optim.Adam(self.critic.parameters(), lr=C_LR)
+        self.criterion_c = nn.MSELoss()
+
+        self.actor_eval = Actor_Net(s_dim, a_dim)
+        self.actor_target = Actor_Net(s_dim, a_dim)
+        self.actor_target.load_state_dict(self.actor_eval.state_dict())
+        self.opt_a = optim.Adam(self.actor_eval.parameters(), lr=A_LR)
+
+        self.distribution = torch.distributions.Normal
+
     def choose_action(self, s):
-        self.eval()
-        mu, sigma = self.forward(s)
+        self.actor_eval.eval()
+        mu, sigma = self.actor_eval(s)
         m = self.distribution(mu.view(1,).data, sigma.view(1,).data)
         a = m.sample().numpy().clip(-2, 2)
         return a
 
+    def update(self, s, a, r):
+        self.actor_eval.train()
+        self.actor_target.load_state_dict(self.actor_eval.state_dict())
 
-class PPO:
-    def __init__(self):
+        adv = r - self.critic(s)
+        mu, sigma = self.actor_eval(s)
+        m_eval = self.distribution(mu, sigma)
+        mu, sigma = self.actor_target(s)
+        m_target = self.distribution(mu, sigma)
+        ratio = m_eval.log_prob(a) / (m_target.log_prob(a)+1e-5)
+        surr = ratio * adv
+        loss_a = -torch.mean(torch.minimum(surr, torch.clip(ratio, 1-EPSILON, 1+EPSILON)*adv))
+        self.opt_a.zero_grad()
+        loss_a.backward()
+        self.opt_a.step()
 
+        loss_c = self.criterion_c(r, self.critic(s))
+        self.opt_c.zero_grad()
+        loss_c.backward()
+        self.opt_c.step()
 
 if __name__ == "__main__":
     env = gym.make('Pendulum-v0').unwrapped
     s_dim = env.observation_space.shape[0]
     a_dim = env.action_space.shape[0]
 
-    ppo = PPO()
+    ppo = PPO(s_dim, a_dim)
+    all_ep_r = []
+
+    for ep in range(EP_MAX):
+        s = env.reset()
+        buf_s, buf_a, buf_r = [], [], []
+        ep_r = 0
+        for t in range(EP_LEN):
+            a = ppo.choose_action(torch.tensor(s[None, :], dtype=torch.float32))
+            s_, r, done, _ = env.step(a)
+            buf_s.append(s)
+            buf_a.append(a)
+            buf_r.append((r+8)/8)
+
+            s = s_
+            ep_r += r
+
+            if (t+1)%BATCH==0 or t==EP_LEN-1:
+                v_s_ = ppo.critic(torch.tensor(s_[None,:], dtype=torch.float32)).data.numpy()[0,0]
+                discounted_r = torch.zeros_like(torch.tensor(buf_r, dtype=torch.float32))
+                for idx in reversed(range(len(buf_r))):
+                    v_s_ = buf_r[idx] + GAMMA * v_s_
+                    discounted_r[idx] = v_s_
+
+                ppo.update(torch.tensor(np.vstack(buf_s), dtype=torch.float32),
+                           torch.tensor(np.vstack(buf_a), dtype=torch.float32),
+                           discounted_r[:, None].clone().detach())
+                buf_s, buf_a, buf_r = [], [], []
+
+        if ep==0:
+            all_ep_r.append(ep_r)
+        else:
+            all_ep_r.append(all_ep_r[-1]*0.9+ep_r*0.1)
+
+        print(f"Ep: {ep}, Ep_r: {ep_r}")
+
+    plt.plot(np.arange(len(all_ep_r)), all_ep_r)
+    plt.xlabel('Episode')
+    plt.ylabel('Moving averaged episode reward')
+    plt.show()
+
+
+
+
+
+
+
     
