7c607b0f04c2574037cb6261219b4ae6abd617c5
Chun-Hsien Huang <n4m4g64b4@gmail.com>
2020-12-06 19:17:44 +0800

    use none morvan version ppo, but learn nothing

--- rl_env/008-PPO/ppo.py
+++ rl_env/008-PPO/ppo.py
@@ -1,229 +1,265 @@
+#!/usr/bin/env python3
+
+'''
+sudo apt install swig -y
+pip install box2d-py
+
+Morvan
+https://github.com/MorvanZhou/Reinforcement-learning-with-tensorflow/blob/master/contents/12_Proximal_Policy_Optimization/simply_PPO.py
+'''
+
+import gym
 import numpy as np
 import torch
 from torch import nn, optim
-import matplotlib.pyplot as plt
-import gym
+from torch.distributions import MultivariateNormal
 
-EP_MAX = 1000
-EP_LEN = 200
-GAMMA = 0.9
-LR = 0.002
-BATCH = 32
-UPDATE_STEPS = 4
-EPSILON=0.2
-
-"""
-1. memory collection
-2. discounted reward
-3. advantage
-4. udpate actor, ppo clip
-5. update critic, MSE(advantage)
-"""
-
-class Actor(nn.Module):
-    def __init__(self, s_dim):
-        super(Actor, self).__init__()
-        self.fc1_a = nn.Linear(s_dim, 100)
-        self.mu = nn.Linear(100, a_dim)
-        self.sigma = nn.Linear(100, a_dim)
-        self.softplus = nn.Softplus()
-        self.relu = nn.ReLU6()
-        self.distribution = torch.distributions.Normal
-        self.set_init(self.fc1_a,
-                      self.mu,
-                      self.sigma)
-
-    def set_init(self, *layers):
-        for layer in layers:
-            layer.weight.data.normal_(0, 0.1)
-            layer.bias.data.fill_(0.)
-
-    def forward(self, s):
-        a = self.fc1_a(s)
-        a = self.relu(a)
-        mu = 2*torch.tanh(self.mu(a))
-        sigma = self.softplus(self.sigma(a))
-        return mu, sigma
-
-    def choose_action(self, s):
-        with torch.no_grad():
-            mu, sigma = self.forward(s)
-            m = self.distribution(mu.view(1), sigma.view(1))
-            a = m.sample()
-            logprob = m.log_prob(a)
-        return a.numpy(), logprob
-
-    def evaluate(self, s, a):
-        mu, sigma = self.forward(s)
-        m = self.distribution(mu, sigma)
-        a_logprob = m.log_prob(a)
-        entropy = m.entropy()
-        return a_logprob, entropy
-
-class Critic(nn.Module):
-    def __init__(self, s_dim, a_dim):
-        super(Critic, self).__init__()
-        self.fc1_c = nn.Linear(s_dim, 100)
-        self.fc2_c = nn.Linear(100, 1)
-        self.relu = nn.ReLU6()
-        self.set_init(self.fc1_c,
-                      self.fc2_c)
-
-    def set_init(self, *layers):
-        for layer in layers:
-            layer.weight.data.normal_(0, 0.1)
-            layer.bias.data.fill_(0.)
-
-    def forward(self, s):
-        s = self.fc1_c(s)
-        s = self.relu(s)
-        v = self.fc2_c(s)
-        return v
-
-    def evaluate(self, s):
-        v = self.forward(s)
-        return v
-
-class PPO:
-    def __init__(self, s_dim, a_dim):
-        self.actor = Actor(s_dim)
-        self.actor_old = Actor(s_dim)
-        self.actor_old.load_state_dict(self.actor.state_dict())
-        self.opt_a = optim.Adam(self.actor.parameters(), LR)
-
-        self.critic = Critic(s_dim, a_dim)
-        self.opt_c = optim.Adam(self.critic.parameters(), LR)
-        self.criterion = nn.MSELoss()
-
-    def update(self, m):
-        s = torch.stack(m.s)
-        a = torch.stack(m.a)
-        logprob = torch.stack(m.logprob)
-
-        r = torch.stack(m.r)
-        discounted_reward = torch.zeros_like(r)
-        tmp_reward = 0
-
-        for idx in reversed(range(len(r))):
-            tmp_reward = r[idx] + GAMMA * tmp_reward
-            discounted_reward[idx] = tmp_reward
-
-        discounted_reward = (discounted_reward-discounted_reward.mean()) / (discounted_reward.std()+1e-5)
-
-        # print(s.shape)
-        # print(a.shape)
-        # print(logprob.shape)
-        # print(discounted_reward.shape)
-        # assert False
-
-        # sample transition
-        # discounted reward
-        # advantage
-        # update actor
-        # update critic
-        
-        for _ in range(len(m)//BATCH):
-            idx = np.random.choice(len(m), BATCH)
-            b_s = s[idx]
-            b_a = a[idx]
-            b_r = discounted_reward[idx]
-            b_logprob = logprob[idx]
-
-            logprobs, entropy = self.actor.evaluate(b_s, b_a)
-            v = self.critic.evaluate(b_s)
-            advantages = b_r-v
-            # surr1
-            ratios = torch.exp(logprobs-b_logprob)
-            surrogate1 = ratios*advantages
-            # surr2
-            surrogate2 = torch.clamp(ratios, 1-EPSILON, 1+EPSILON)*advantages
-            loss_a = -torch.mean(torch.minimum(surrogate1, surrogate2))
-            self.opt_a.zero_grad()
-            loss_a.backward()
-            self.opt_a.step()
-
-            v = self.critic.evaluate(b_s)
-            loss_c = self.criterion(b_r, v)
-            self.opt_c.zero_grad()
-            loss_c.backward()
-            self.opt_c.step()
-
-        self.actor_old.load_state_dict(self.actor.state_dict())
 
 class Memory:
     def __init__(self):
-        self.s = []
-        self.a = []
-        self.r = []
-        self.logprob = []
+        self.states = []
+        self.actions = []
+        self.logprobs = []
+        self.rewards = []
+        self.is_terminals = []
 
-    def save(self, s, a, r, logprob):
-        # save as tensor
-        self.s.append(torch.Tensor(s))
-        self.a.append(torch.Tensor(a))
-        self.r.append(torch.Tensor([r]))
-        self.logprob.append(logprob)
+    def clear_memory(self):
+        self.states.clear()
+        self.actions.clear()
+        self.logprobs.clear()
+        self.rewards.clear()
+        self.is_terminals.clear()
 
-    def clean(self):
-        self.s = []
-        self.a = []
-        self.r = []
-        self.logprob = []
 
-    def __len__(self):
-        return len(self.s)
+class ActorCritic(nn.Module):
+    def __init__(self, s_dim, a_dim, action_std, device):
+        super(ActorCritic, self).__init__()
+        self.actor = nn.Sequential(
+                nn.Linear(s_dim, 64),
+                nn.Tanh(),
+                nn.Linear(64, 32),
+                nn.Tanh(),
+                nn.Linear(32, a_dim),
+                nn.Tanh())
+        self.critic = nn.Sequential(
+                nn.Linear(s_dim, 64),
+                nn.Tanh(),
+                nn.Linear(64, 32),
+                nn.Tanh(),
+                nn.Linear(32, 1))
+        self.action_var = torch.full(size=(a_dim,),
+                                     fill_value=action_std*action_std,
+                                     device=device)
+        self.device = device
 
-if __name__ == "__main__":
-    env = gym.make('Pendulum-v0').unwrapped
+    def forward(self):
+        raise NotImplementedError
+
+    def act(self, state, memory):
+        # state.shape = (1, s_dim)
+        action_mean = self.actor(state)
+        # action_mean.shape = (1, a_dim)
+        cov_mat = torch.diag(self.action_var).to(self.device)
+        # cov_mat.shape = (a_dim, a_dim)
+
+        dist = MultivariateNormal(action_mean, cov_mat)
+        action = dist.sample()
+        # action.shape = (1, a_dim)
+        action_logprob = dist.log_prob(action)
+        # action_logprob.shape = (1,)
+
+        memory.states.append(state.detach().cpu().numpy())
+        memory.actions.append(action.detach().cpu().numpy())
+        memory.logprobs.append(action_logprob.detach().cpu().numpy())
+
+        return action.detach()
+
+    def evaluate(self, state, action):
+        # state.shape = (batch_size, s_dim)
+        # action.shape = (batch_size, a_dim)
+        action_mean = self.actor(state)
+        # action_mean.shape = (batch_size, a_dim)
+        action_var = self.action_var.expand_as(action_mean)
+        # action_var.shape = (batch_size, a_dim)
+        cov_mat = torch.diag_embed(action_var).to(self.device)
+
+        dist = MultivariateNormal(action_mean, cov_mat)
+        action_logprob = dist.log_prob(action)
+        # action_logprob.shape = (batch_size,)
+        dist_entropy = dist.entropy()
+        # dist_entropy.shape = (batch_size,)
+
+        state_value = self.critic(state)
+
+        return action_logprob, torch.squeeze(state_value), dist_entropy
+
+
+class PPO:
+    def __init__(self, s_dim, a_dim, action_std, lr,
+                 betas, gamma, K_epochs, eps_clip, device):
+        self.lr = lr
+        self.betas = betas
+        self.gamma = gamma
+        self.eps_clip = eps_clip
+        self.K_epochs = K_epochs
+        self.device = device
+
+        self.policy = ActorCritic(s_dim,
+                                  a_dim,
+                                  action_std,
+                                  device).to(device)
+        self.optimizer = optim.Adam(self.policy.parameters(),
+                                    lr=lr,
+                                    betas=betas)
+        self.policy_old = ActorCritic(s_dim,
+                                      a_dim,
+                                      action_std,
+                                      device).to(device)
+        self.policy_old.load_state_dict(self.policy.state_dict())
+        self.criterion = nn.MSELoss()
+
+    def choose_action(self, state, memory):
+        # state : list
+        # state.shape = (s_dim,)
+        with torch.no_grad():
+            state = torch.tensor(state,
+                                 dtype=torch.float32,
+                                 device=self.device).unsqueeze(0)
+            # state.shape = (1, s_dim)
+            action = self.policy_old.act(state, memory)
+            action = action.squeeze(0).cpu().numpy()
+            # action : numpy array
+            # action.shape = (a_dim,)
+        return action
+
+    def update(self, memory):
+        rewards = [0]*len(memory.rewards)
+        disc_reward = 0
+        for idx in reversed(range(len(rewards))):
+            if memory.is_terminals[idx]:
+                disc_reward = 0
+            disc_reward = memory.rewards[idx] + self.gamma * disc_reward
+            rewards[idx] = disc_reward
+
+        rewards = torch.tensor(rewards,
+                               dtype=torch.float32,
+                               device=self.device)
+        # rewards.shape = (len(memory.rewards),)
+        rewards = (rewards - rewards.mean()) / (rewards.std() + 1e-5)
+        # rewards.shape = (len(memory.rewards),)
+
+        old_states = torch.tensor(np.vstack(memory.states),
+                                  dtype=torch.float32,
+                                  device=self.device)
+        # old_states.shape = (batch_size, s_dim)
+        old_actions = torch.tensor(np.vstack(memory.actions),
+                                   dtype=torch.float32,
+                                   device=self.device)
+        # old_actions.shape = (batch_size, a_dim)
+        old_logprobs = torch.tensor(np.vstack(memory.logprobs),
+                                    dtype=torch.float32,
+                                    device=self.device)
+        # old_logprobs.shape = (batch_size, 1)
+
+        for _ in range(self.K_epochs):
+            result = self.policy.evaluate(old_states, old_actions)
+            logprobs, state_values, dist_entropy = result
+
+            ratios = torch.exp(logprobs - old_logprobs.detach())
+
+            advantages = rewards - state_values.detach()
+            surr1 = ratios * advantages
+            surr2 = torch.clip(ratios,
+                               1-self.eps_clip,
+                               1+self.eps_clip) * advantages
+
+            loss = (-torch.min(surr1, surr2) +
+                    0.5 * self.criterion(state_values, rewards) -
+                    0.01 * dist_entropy)
+
+            self.optimizer.zero_grad()
+            loss.mean().backward()
+            self.optimizer.step()
+
+        self.policy_old.load_state_dict(self.policy.state_dict())
+
+
+def main():
+    env_name = 'BipedalWalker-v2'
+    render = True
+    solved_reward = 300
+    log_interval = 20
+    max_episodes = 10000
+    max_timesteps = 1500
+
+    update_timestep = 4000
+    action_std = 0.5
+    K_epochs = 80
+    eps_clip = 0.2
+    gamma = 0.99
+
+    lr = 3e-4
+    betas = (0.9, 0.999)
+
+    env = gym.make(env_name)
     s_dim = env.observation_space.shape[0]
     a_dim = env.action_space.shape[0]
-    print(f"s_dim: {s_dim}, a_dim: {a_dim}")
+    # s_dim, a_dim = 24, 4
 
-    ppo = PPO(s_dim, a_dim)
-    all_ep_r = []
+    device = torch.device('cuda')
 
-    for ep in range(EP_MAX):
-        s = env.reset()
-        m = Memory()
-        ep_r = 0
-        step = 0
-        for t in range(EP_LEN):
-            a, logprob = ppo.actor_old.choose_action(torch.tensor(s[None, :], dtype=torch.float32))
-            s_, r, done, _ = env.step(a)
-            # print("s, a, s_, r, logprob")
-            # # list, list, list, scalor, tensor
-            # print(s, a, s_, r, logprob)
-            # print(s.shape, a.shape, s_.shape, r.shape, logprob.shape)
-            # assert False
-            m.save(s, a, (r+8)/8, logprob.detach())
+    memory = Memory()
+    ppo = PPO(s_dim, a_dim, action_std, lr, betas,
+              gamma, K_epochs, eps_clip, device)
 
-            s = s_
-            ep_r += r
-            step += 1
+    running_reward = 0
+    avg_length = 0
+    time_step = 0
 
-            if (t+1)%(BATCH*10)==0 or t==EP_LEN-1:
-                ppo.update(m)
-                m.clean()
+    for eps in range(max_episodes):
+        state = env.reset()
+        for t in range(max_timesteps):
+            time_step += 1
+            action = ppo.choose_action(state, memory)
+            # action.shape = (a_dim,)
+            state, reward, done, _ = env.step(action)
+
+            memory.rewards.append(reward)
+            memory.is_terminals.append(done)
+
+            if time_step % update_timestep == 0:
+                ppo.update(memory)
+                memory.clear_memory()
+                time_step = 0
+
+            running_reward += reward
+
+            if render:
+                env.render()
 
             if done:
                 break
 
-        if ep==0:
-            all_ep_r.append(ep_r)
-        else:
-            all_ep_r.append(all_ep_r[-1]*0.9+ep_r*0.1)
+        avg_length += t
 
-        print(f"Ep: {ep}, Ep_r: {ep_r:.3f}")
+        if running_reward > log_interval * solved_reward:
+            print("### Solved! ###")
+            torch.save(ppo.policy.state_dict(), 'ppo_solved.pt')
+            break
 
-    plt.plot(np.arange(len(all_ep_r)), all_ep_r)
-    plt.xlabel('Episode')
-    plt.ylabel('Moving averaged episode reward')
-    plt.show()
+        if (eps+1) % 500 == 0:
+            torch.save(ppo.policy.state_dict(), 'ppo.pt')
+
+        if (eps+1) % log_interval == 0:
+            avg_length = int(avg_length/log_interval)
+            running_reward = int(running_reward/log_interval)
+
+            print(f"Episode {eps+1}")
+            print(f"\tAvg length: {avg_length}")
+            print(f"\tAvg reward: {running_reward}")
+            running_reward = 0
+            avg_length = 0
 
 
-
-
-
-
-
-    
+if __name__ == "__main__":
+    main()
