84e74146ff2bbe055d865863c89d56555b7b734a
Chun-Hsien Huang <n4m4g64b4@gmail.com>
2020-12-10 13:44:37 +0800

    use pynvml to access gpu info

---
+++ nvml_env/nv_info.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from pynvml import nvmlInit
+from pynvml import nvmlSystemGetDriverVersion
+from pynvml import nvmlDeviceGetHandleByIndex
+from pynvml import nvmlDeviceGetName
+from pynvml import nvmlDeviceGetUtilizationRates
+from pynvml import nvmlDeviceGetMemoryInfo
+from pynvml import nvmlDeviceGetTemperature
+from pynvml import nvmlDeviceGetFanSpeed
+from pynvml import nvmlDeviceGetPowerUsage
+from pynvml import nvmlDeviceGetPowerManagementDefaultLimit
+from pynvml import nvmlDeviceGetDecoderUtilization
+from pynvml import nvmlShutdown
+
+
+if __name__ == "__main__":
+    nvmlInit()
+    handle = nvmlDeviceGetHandleByIndex(0)
+
+    # device name, driver version
+    driver_version = nvmlSystemGetDriverVersion().decode()
+    device_name = nvmlDeviceGetName(handle).decode()
+    print("GPU {}: {}, {}".format(0, device_name, driver_version))
+
+    # utilize
+    utilization = nvmlDeviceGetUtilizationRates(handle)
+    print("GPU-MEM: \t{}% / {}%".format(utilization.gpu,
+                                        utilization.memory))
+
+    # memory
+    info = nvmlDeviceGetMemoryInfo(handle)
+    mem_total = int(info.total/1024/1024)
+    mem_used = int(info.used/1024/1024)
+    print("Memory: \t{}MiB / {}MiB".format(mem_used, mem_total))
+
+    # temperature
+    temp = nvmlDeviceGetTemperature(handle, 0)
+    print("Temperature: \t{}C".format(temp))
+
+    # fan speed
+    fan_speed = nvmlDeviceGetFanSpeed(handle)
+    print("Fan speed: \t{}%".format(fan_speed))
+
+    # power state
+    power_used = int(nvmlDeviceGetPowerUsage(handle)/1000)
+    power_limit = int(nvmlDeviceGetPowerManagementDefaultLimit(handle)/1000)
+    print("Power state: \t{}W / {}W".format(power_used, power_limit))
+
+    # decoder usage
+    utilization, samplingPeriodUs = nvmlDeviceGetDecoderUtilization(handle)
+    print("Decoder used: \t{}%".format(utilization))
+
+    print()
+
+    nvmlShutdown()
