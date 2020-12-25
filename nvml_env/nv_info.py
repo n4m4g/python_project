ce9a2f2fd7d76ca422875448332b2ece2275a90c
Chun-Hsien Huang <n4m4g64b4@gmail.com>
2020-12-25 16:22:49 +0800

    add indent to each line

--- nvml_env/nv_info.py
+++ nvml_env/nv_info.py
@@ -37,44 +37,44 @@ if __name__ == "__main__":
 
         # driver version
         driver_version = nvmlSystemGetDriverVersion().decode()
-        print("Driver Version: {}".format(driver_version))
+        print("\tDriver Version: {}".format(driver_version))
 
         # clock
         clk = nvmlDeviceGetClockInfo(handle, 0)
         max_clk = nvmlDeviceGetMaxClockInfo(handle, 0)
-        print("GPU Clock: \t{}MHz / {}MHz".format(clk, max_clk))
+        print("\tGPU Clock: \t{}MHz / {}MHz".format(clk, max_clk))
 
         # utilize
         utilization = nvmlDeviceGetUtilizationRates(handle)
-        print("GPU Util: \t{}%".format(utilization.gpu))
-        print("MEM Util: \t{}%".format(utilization.memory))
+        print("\tGPU Util: \t{}%".format(utilization.gpu))
+        print("\tMEM Util: \t{}%".format(utilization.memory))
 
         # power state
         power_used = nvmlDeviceGetPowerUsage(handle)/1000
         power_limit = nvmlDeviceGetPowerManagementDefaultLimit(handle)/1000
-        print("Power Util: \t{}W / {}W".format(int(power_used),
-                                               int(power_limit)))
+        print("\tPower Util: \t{}W / {}W".format(int(power_used),
+                                                 int(power_limit)))
 
         # decoder usage
         utilization, samplingPeriodUs = nvmlDeviceGetDecoderUtilization(handle)
-        print("Decoder Util: \t{}%".format(utilization))
+        print("\tDecoder Util: \t{}%".format(utilization))
 
         # memory
         info = nvmlDeviceGetMemoryInfo(handle)
         mem_used = toMiB(info.used)
         mem_free = toMiB(info.free)
         mem_total = toMiB(info.total)
-        print("Memory: \t{}MiB / {}MiB / {}MiB".format(mem_used,
-                                                       mem_free,
-                                                       mem_total))
+        print("\tMemory: \t{}MiB / {}MiB / {}MiB".format(mem_used,
+                                                         mem_free,
+                                                         mem_total))
 
         # fan speed
         fan_speed = nvmlDeviceGetFanSpeed(handle)
-        print("Fan Speed: \t{}%".format(fan_speed))
+        print("\tFan Speed: \t{}%".format(fan_speed))
 
         # temperature
         temp = nvmlDeviceGetTemperature(handle, 0)
-        print("Temperature: \t{}C".format(temp))
+        print("\tTemperature: \t{}C".format(temp))
 
         # processes
         compute_processes = nvmlDeviceGetComputeRunningProcesses(handle)
@@ -82,12 +82,12 @@ if __name__ == "__main__":
         graphic_processes = sorted(graphic_processes,
                                    key=lambda x: toMiB(x.usedGpuMemory),
                                    reverse=True)
-        print("\n=== Processes ===")
+        print("\n\t=== Processes ===")
         for p in graphic_processes:
             p_pid = p.pid
             p_name = nvmlSystemGetProcessName(p_pid).decode()
             p_name = p_name.split(' ')[0].split('/')[-1]
             p_mem = toMiB(p.usedGpuMemory)
-            print("{}: [{}MiB]".format(p_name, p_mem))
+            print("\t{}: [{}MiB]".format(p_name, p_mem))
 
     nvmlShutdown()
